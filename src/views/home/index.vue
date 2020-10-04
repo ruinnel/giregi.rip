263a18f64b504813c2c9b55a8e3e2a7bbf56a0bf
ruinnel <ruinnel@gmail.com>
2020-10-04 16:41:28 +0900

    url input - component

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,66 +1,32 @@
 <template>
-  <validation-observer ref="validator">
-    <div class="container-xl">
-      <!-- Page title -->
-      <div class="page-header text-white">
-        <div class="row align-items-center">
-          <div class="col-auto">
-            <h2 class="page-title">
-              Giregi R.I.P
-              <i class="fas fa-grip-lines-vertical" />
-              <span class="small">기억하기 보다 기록하자.</span>
-            </h2>
-          </div>
+  <div class="container-xl">
+    <!-- Page title -->
+    <div class="page-header text-white">
+      <div class="row align-items-center">
+        <div class="col-auto">
+          <h2 class="page-title">
+            Giregi R.I.P
+            <i class="fas fa-archive pl-3" />
+            <span class="small">기억하기 보다 기록하자.</span>
+          </h2>
         </div>
-      </div>
-      <div class="row row-deck row-cards">
-        <div class="col-sm-12 col-lg-12">
-          <div class="card">
-            <div class="card-body">
-              <label class="form-label" for="url-input">아카이브 할 URL을 입력해 주세요.</label>
-              <validator rules="required|url" name="URL">
-                <div class="input-group input-group-flat">
-                  <input
-                    id="url-input"
-                    v-model="url"
-                    type="text"
-                    class="form-control is-valid-lite"
-                    @keyup.enter="loadPreview"
-                  >
-                  <div v-if="url" class="input-group-append">
-                    <span class="input-group-text">
-                      <a class="link-secondary" title="Clear search" data-toggle="tooltip" @click="clearUrl">
-                        <i class="fas fa-times" />&nbsp;
-                      </a>
-                    </span>
-                  </div>
-                </div>
-              </validator>
-
-              <div class="align-items-center mt-2">
-                <button class="btn btn-outline-primary btn-block" :disabled="!isUrl(url)" @click="loadPreview">
-                  <i class="far fa-eye" />
-                  미리보기
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <preview :preview="preview" @archive="onArchive" />
       </div>
     </div>
-  </validation-observer>
+    <url-input v-model="url" @preview="loadPreview" />
+    <preview v-if="showPreview" :preview="preview" @archive="onArchive" />
+  </div>
 </template>
 
 <script>
 import { isEmpty } from 'lodash';
-import isURL from 'validator/lib/isURL';
 import ApiClient, { API } from 'api/client';
 import Preview from './modules/Preview';
+import UrlInput from 'views/home/modules/UrlInput';
 
 export default {
   name: 'Home',
   components: {
+    UrlInput,
     Preview,
   },
   mixins: [ApiClient],
@@ -70,7 +36,11 @@ export default {
       preview: {},
     };
   },
-  computed: {},
+  computed: {
+    showPreview() {
+      return !isEmpty(this.preview);
+    },
+  },
   methods: {
     async loadPreview() {
       const loader = this.$loading.show();
@@ -87,12 +57,6 @@ export default {
       } finally {
         loader.hide();
       }
-    },
-    isUrl(url) {
-      return isURL(url);
-    },
-    clearUrl() {
-      this.url = '';
     },
     clear() {
       this.url = '';
---
+++ src/views/home/modules/UrlInput.vue
@@ -0,0 +1,79 @@
+<template>
+  <validation-observer ref="validator">
+    <div class="row row-deck row-cards">
+      <div class="col-sm-12 col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <label class="form-label" for="url-input">아카이브 할 URL을 입력해 주세요.</label>
+            <validator rules="required|url" name="URL">
+              <div class="input-group input-group-flat">
+                <input
+                  id="url-input"
+                  v-model="url"
+                  type="text"
+                  class="form-control is-valid-lite"
+                  @keyup.enter="preview"
+                >
+                <div v-if="url" class="input-group-append">
+                  <span class="input-group-text">
+                    <a class="link-secondary" title="Clear search" data-toggle="tooltip" @click="clearUrl">
+                      <i class="fas fa-times pl-2 pr-2" />
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </validator>
+
+            <div class="align-items-center mt-2">
+              <button class="btn btn-outline-primary btn-block" :disabled="!isUrl(url)" @click="preview">
+                <i class="far fa-eye" />
+                미리보기
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </validation-observer>
+</template>
+
+<script>
+import isURL from 'validator/lib/isURL';
+export default {
+  name: 'UrlInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      url: '',
+    };
+  },
+  watch: {
+    url() {
+      this.$emit('input', this.url);
+    },
+  },
+  methods: {
+    isUrl(url) {
+      return isURL(url);
+    },
+    clearUrl() {
+      this.url = '';
+    },
+    preview() {
+      const valid = this.$refs.validator.validate();
+      if (valid) {
+        this.$emit('preview', this.url);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
